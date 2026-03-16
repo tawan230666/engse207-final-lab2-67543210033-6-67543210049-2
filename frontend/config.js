176da13c1aef5d9c4fb6eaf49610a67712bfb37b
tawan230666 <tawantippkun200501@gmail.com>
2026-03-16 14:46:09 +0700

    chore: update production service URLs

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL: 'http://localhost:3001',
-  TASK_URL: 'http://localhost:3002',
-  USER_URL: 'http://localhost:3003'
+  AUTH_URL: 'auth-service-production-1320.up.railway.app',
+  TASK_URL: 'task-service-production-2930.up.railway.app',
+  USER_URL: 'user-service-production-901e.up.railway.app' // อันนี้ตามรูปที่ส่งมา
 };
