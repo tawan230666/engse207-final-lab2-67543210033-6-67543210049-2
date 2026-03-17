5e24eeb6c5639a02a5a3cc3aa23599d16eb675eb
tawan230666 <tawantippkun200501@gmail.com>
2026-03-18 00:16:35 +0700

    fix: update config.js to support both Local and Railway URLs

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,7 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
 window.APP_CONFIG = {
-  AUTH_URL: isLocal ? 'http://localhost:3001' : 'https://auth-service-production-7268.up.railway.app',
-  TASK_URL: isLocal ? 'http://localhost:3002' : 'https://task-service-production-747e.up.railway.app',
-  USER_URL: isLocal ? 'http://localhost:3003' : 'https://user-service-production-57b1.up.railway.app'
+  AUTH_URL: isLocal ? 'http://localhost:3001' : 'https://auth-service-production-1320.up.railway.app',
+  TASK_URL: isLocal ? 'http://localhost:3002' : 'https://task-service-production-2930.up.railway.app', 
+  USER_URL: isLocal ? 'http://localhost:3003' : 'https://user-service-production-901e.up.railway.app'
 };
